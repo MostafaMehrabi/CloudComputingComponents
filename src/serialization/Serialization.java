aef4bdd95454d4cd3beeda06f8b2ebd956ae0fb3
MostafaMehrabi <mehrabimostafa@gmail.com>
2018-02-08 21:27:06 +1300

    trying to serialize, not successful so far

--- src/serialization/Serialization.java
+++ src/serialization/Serialization.java
@@ -1,5 +1,9 @@
 package serialization;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Serialization {
 	
 	ClassOne classOne = null;
@@ -19,5 +23,27 @@ public class Serialization {
 		String st1 = "this new string is: ";
 		st1 += varOne + "_" + varTwo.getValue();
 		System.out.println("the result is: " + st1); 
-	}	
+	}
+	
+	private void writeObject(final ObjectOutputStream out) throws IOException{
+		try {
+			System.out.println("Write object called");
+			out.defaultWriteObject();
+			out.writeObject(classOne);
+			out.writeObject(classTwo);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void readObject(final ObjectInputStream in) throws IOException{
+		try {
+			in.defaultReadObject();
+			classOne = (ClassOne) in.readObject();
+			classTwo = (ClassTwo) in.readObject();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
