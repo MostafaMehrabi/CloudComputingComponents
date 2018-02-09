238fcfbe200d7a41bf971c174ef5032c0668de52
MostafaMehrabi <mehrabimostafa@gmail.com>
2018-02-09 19:07:19 +1300

    Proposing protocol for naming the created classes based on the invoked method, its signature and its enclosing class, such that we consider overloaded methods

---
+++ src/serialization/server/CloudTask_ClassName_MethodName_Arg1Type_Arg2Type.java
@@ -0,0 +1,15 @@
+package serialization.server;
+
+import java.io.Serializable;
+
+import serialization.CloudTaskWithReturn;
+
+public class CloudTask_ClassName_MethodName_Arg1Type_Arg2Type extends CloudTaskWithReturn<String> {
+	private static final long serialVersionUID = 1L;
+	@Override
+	public String method() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
