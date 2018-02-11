a9b009da6bcf11abb39df12e779ebb0504eb523d
Mostafa Mehrabi <mehrabimostafa@gmail.com>
2018-02-11 17:27:47 +1300

    managed to transfer objects over socket/http connections, but the problem is that this won't work without dynamic class loading

--- src/serialization/client/Client.java
+++ src/serialization/client/Client.java
@@ -1,5 +1,47 @@
 package serialization.client;
 
-public class Client {
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
+import serialization.ClassTwo;
+import serialization.CustomizedCloudTask;
+
+public class Client {
+	private final int portNumber = 8080;
+	
+	public Client() {
+		
+	}
+	
+	public void connectWithServer() {
+		try {
+			Socket socket = new Socket("localhost", portNumber);
+			ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+			
+			//create a cloud task and turn it into byte array
+			CustomizedCloudTask cloudTask = new CustomizedCloudTask(new ClassTwo("william", 32), new ClassTwo("Mark", 25), 13);
+			ByteArrayOutputStream byteArrayOutput = new ByteArrayOutputStream();
+			ObjectOutputStream objectReader = new ObjectOutputStream(byteArrayOutput);
+			objectReader.writeObject(cloudTask);
+			objectReader.flush();
+			byte[] taskBytes = byteArrayOutput.toByteArray();
+			byteArrayOutput.flush();
+			objectReader.close();
+			byteArrayOutput.close();
+			
+			//send the byte array to server.
+			output.writeObject(cloudTask);
+			output.flush();
+			output.close();
+			socket.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.connectWithServer();
+	}
 }
--- src/serialization/server/Server.java
+++ src/serialization/server/Server.java
@@ -1,5 +1,64 @@
 package serialization.server;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import serialization.CloudTask;
+
+public class Server {
+	
+	private final int portNummber = 8080;
+	
+	public Server() {
+		
+	}
+	
+	public CloudTask startServer() {
+		ServerSocket serverSocket = null;
+		Socket clientSocket = null;
+		try {
+			serverSocket = new ServerSocket(portNummber);
+			clientSocket = serverSocket.accept(); 
+			InputStream input = clientSocket.getInputStream();
+			ObjectInputStream objectInput = new ObjectInputStream(input);
+			
+//			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+//			byte[] data = new byte[1024];
+//			int readData = 0;
+//			while((readData = input.read(data, 0, data.length)) != -1) {
+//				buffer.write(data, 0, readData);
+//			}
+//			buffer.flush();
+//			byte[] readBytes = buffer.toByteArray();
+//			ByteArrayInputStream inputByteArray = new ByteArrayInputStream(readBytes);
+//			ObjectInputStream objectInputStream = new ObjectInputStream(inputByteArray);
+			Object obj = objectInput.readObject();
+			System.out.println(obj.getClass());
+			CloudTask task = (CloudTask) obj;
+			serverSocket.close();
+			clientSocket.close();
+			return task;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void doTask(CloudTask task) {
+		task.doWork();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Server started its job");
+		Server server = new Server();
+		CloudTask task = server.startServer();
+		server.doTask(task);
+		System.out.println("Server finished its job");
+	}
 }
